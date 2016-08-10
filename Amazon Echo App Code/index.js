ef4e976e5ef44a9a7e7119f10728bb27497f78cb
cianohagan <cian.ohagan5@mail.dcu.ie>
2016-08-10 15:17:50 +0100

    updating alexa code to handle no data and tomorrow's lunch intent

--- Amazon Echo App Code/index.js
+++ Amazon Echo App Code/index.js
@@ -1,3 +1,4 @@
+// Amazon Echo skill that returns lunch options to user on request
 var alexa = require('alexa-app');
 var slug = require('slug');
 
@@ -29,19 +30,80 @@ app.intent('lunchIntent', {
     client.get(url, function (lunch, callback) {
         // parsed response body as js object
         data = JSON.parse(lunch);
-        var mainDishOne = data.main1name.replace("&", "and");
-        var mainDishTwo = data.main2name.replace("&", "and");
-        var mainDishVeg = data.vegname.replace("&", "and");
-        var soupOne = data.soup1name.replace("&", "and");
-        var soupTwo = data.soup2name.replace("&", "and");
-        console.log(mainDishOne, mainDishTwo, mainDishVeg, soupOne, soupTwo);
-        speechOutput = "The main dishes today are " + mainDishOne + " and " + mainDishTwo + ". The vegetarian option is " + mainDishVeg + ". Today's soups are " + soupOne + " and " + soupTwo;
-        
-        console.log(speechOutput);
-        // This is async and will run after the http call returns
-        response.say(speechOutput).shouldEndSession(true);
-        // Must call send to end the original request 
-        response.send(); 
+        if (data != "Data Unavailable"){
+            var mainDishOne = data.main1name.replace("&", "and");
+            var mainDishTwo = data.main2name.replace("&", "and");
+            var mainDishVeg = data.vegname.replace("&", "and");
+            var soupOne = data.soup1name.replace("&", "and");
+            var soupTwo = data.soup2name.replace("&", "and");
+            console.log(mainDishOne, mainDishTwo, mainDishVeg, soupOne, soupTwo);
+            speechOutput = "The main dishes today are " + mainDishOne + " and " + mainDishTwo + ". The vegetarian option is " + mainDishVeg + ". Today's soups are " + soupOne + " and " + soupTwo;
+            
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }
+        else{
+            speechOutput = "Today's menu has not been uploaded yet. Please try again later"
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }  
+           
+    }).on('error', function (err) {
+        console.log('something went wrong on the request', err.request.options);
+    });    
+
+    // Return false immediately so alexa-app doesn't send the response
+    return false;
+});
+
+app.intent('tomorrowLunchIntent', {
+        "utterances": [
+            "What is tomorrow's lunch",
+            "What's tomorrow's lunch",
+            "What is being served in the canteen tomorrow",
+            "tomorrow",
+            "tomorrow's menu",
+            "tomorrow's lunch",
+            "tomorrow lunch",
+            "tomorrow menu",
+            "What is for lunch tomorrow"
+        ]
+    }, function(request, response) {
+
+    var url = "https://whatsforlunchapp.herokuapp.com/tomorrowMenu";
+
+    client.get(url, function (lunch, callback) {
+        // parsed response body as js object
+        data = JSON.parse(lunch);
+        if (data != "Data Unavailable"){
+            var mainDishOne = data.main1name.replace("&", "and");
+            var mainDishTwo = data.main2name.replace("&", "and");
+            var mainDishVeg = data.vegname.replace("&", "and");
+            var soupOne = data.soup1name.replace("&", "and");
+            var soupTwo = data.soup2name.replace("&", "and");
+            console.log(mainDishOne, mainDishTwo, mainDishVeg, soupOne, soupTwo);
+            speechOutput = "Tomorrow's main dishes are " + mainDishOne + " and " + mainDishTwo + ". The vegetarian option is " + mainDishVeg + ". The soups will be " + soupOne + " and " + soupTwo;
+            
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }
+        else{
+            speechOutput = "Tomorrow's menu has not been uploaded yet. Please try again later"
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        } 
            
     }).on('error', function (err) {
         console.log('something went wrong on the request', err.request.options);
@@ -65,26 +127,36 @@ app.intent('soupIntent', {
     client.get(url, function (lunch, callback) {
         // parsed response body as js object
         data = JSON.parse(lunch);
-        slug.charmap['€'] = ''
-        slug.charmap['.'] = 'euro'
-        var soupOne = data.soup1name.replace("&", "and");
-        var soupTwo = data.soup2name.replace("&", "and");
-        var soupOnePrice = slug(data.soup1price);
-        var soupTwoPrice = slug(data.soup2price);
-        console.log(soupOne, soupTwo, soupOnePrice);
-        if(soupOnePrice === soupTwoPrice){
-            speechOutput = "Today's soups are " + soupOne + " and " + soupTwo + ". They are both priced at " + soupOnePrice;
+        if (data != "Data Unavailable"){
+            slug.charmap['€'] = ''
+            slug.charmap['.'] = 'euro'
+            var soupOne = data.soup1name.replace("&", "and");
+            var soupTwo = data.soup2name.replace("&", "and");
+            var soupOnePrice = slug(data.soup1price);
+            var soupTwoPrice = slug(data.soup2price);
+            console.log(soupOne, soupTwo, soupOnePrice);
+            if(soupOnePrice === soupTwoPrice){
+                speechOutput = "Today's soups are " + soupOne + " and " + soupTwo + ". They are both priced at " + soupOnePrice;
+            }
+            else{
+                speechOutput = "Today's soups are " + soupOne + " and " + soupTwo + ". The " + soupOne + " is priced at " + soup1price + " while the " + soupTwo + " is " + soupTwoPrice;
+            }
+            
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send(); 
         }
         else{
-            speechOutput = "Today's soups are " + soupOne + " and " + soupTwo + ". The " + soupOne + " is priced at " + soup1price + " while the " + soupTwo + " is " + soupTwoPrice;
-        }
-        
-        console.log(speechOutput);
-        // This is async and will run after the http call returns
-        response.say(speechOutput).shouldEndSession(true);
-        // Must call send to end the original request 
-        response.send(); 
-           
+            speechOutput = "Today's menu has not been uploaded yet. Please try again later"
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        } 
+
     }).on('error', function (err) {
         console.log('something went wrong on the request', err.request.options);
     });    
@@ -107,25 +179,35 @@ app.intent('meatIntent', {
     client.get(url, function (lunch, callback) {
         // parsed response body as js object
         data = JSON.parse(lunch);
-        slug.charmap['€'] = ''
-        slug.charmap['.'] = 'euro'
-        var mainDishOne = data.main1name.replace("&", "and");
-        var mainDishTwo = data.main2name.replace("&", "and");
-        var mainOnePrice = slug(data.main1price);
-        var mainTwoPrice = slug(data.main2price);
-        console.log(mainDishOne, mainDishTwo, mainOnePrice);
-        if(mainOnePrice === mainTwoPrice){
-            speechOutput = "Today's meat dishes are " + mainDishOne + " and " + mainDishTwo + ". They are both priced at " + mainOnePrice;
+        if (data != "Data Unavailable"){
+            slug.charmap['€'] = ''
+            slug.charmap['.'] = 'euro'
+            var mainDishOne = data.main1name.replace("&", "and");
+            var mainDishTwo = data.main2name.replace("&", "and");
+            var mainOnePrice = slug(data.main1price);
+            var mainTwoPrice = slug(data.main2price);
+            console.log(mainDishOne, mainDishTwo, mainOnePrice);
+            if(mainOnePrice === mainTwoPrice){
+                speechOutput = "Today's meat dishes are " + mainDishOne + " and " + mainDishTwo + ". They are both priced at " + mainOnePrice;
+            }
+            else{
+                speechOutput = "Today's meat dishes are " + mainDishOne + " and " + mainDishTwo + ". The " + mainDishOne + " is priced at " + mainOnePrice + " while the " + mainDishTwo + " is " + mainTwoPrice;
+            }
+            
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send(); 
         }
         else{
-            speechOutput = "Today's meat dishes are " + mainDishOne + " and " + mainDishTwo + ". The " + mainDishOne + " is priced at " + mainOnePrice + " while the " + mainDishTwo + " is " + mainTwoPrice;
-        }
-        
-        console.log(speechOutput);
-        // This is async and will run after the http call returns
-        response.say(speechOutput).shouldEndSession(true);
-        // Must call send to end the original request 
-        response.send(); 
+            speechOutput = "Today's menu has not been uploaded yet. Please try again later"
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        } 
            
     }).on('error', function (err) {
         console.log('something went wrong on the request', err.request.options);
@@ -149,20 +231,30 @@ app.intent('vegIntent', {
     client.get(url, function (lunch, callback) {
         // parsed response body as js object
         data = JSON.parse(lunch);
-        slug.charmap['€'] = ''
-        slug.charmap['.'] = 'euro'
-        var mainDishVeg = data.vegname.replace("&", "and");
-        var vegPrice = slug(data.vegprice);
-        console.log(mainDishVeg, vegPrice);
+        if (data != "Data Unavailable"){
+            slug.charmap['€'] = ''
+            slug.charmap['.'] = 'euro'
+            var mainDishVeg = data.vegname.replace("&", "and");
+            var vegPrice = slug(data.vegprice);
+            console.log(mainDishVeg, vegPrice);
 
-        speechOutput = "Today's vegetarian dish is " + mainDishVeg + ". It is priced at " + vegPrice;
+            speechOutput = "Today's vegetarian dish is " + mainDishVeg + ". It is priced at " + vegPrice;
 
-        
-        console.log(speechOutput);
-        // This is async and will run after the http call returns
-        response.say(speechOutput).shouldEndSession(true);
-        // Must call send to end the original request 
-        response.send(); 
+            
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }
+        else{
+            speechOutput = "Today's menu has not been uploaded yet. Please try again later"
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }  
            
     }).on('error', function (err) {
         console.log('something went wrong on the request', err.request.options);
@@ -184,19 +276,29 @@ app.intent('tellMeMoreVegIntent', {
     client.get(url, function (lunch, callback) {
         // parsed response body as js object
         data = JSON.parse(lunch);
-        var mainDishVeg = data.vegname.replace("&", "and");
-        var originalDesc = data.vegdesc;
-        var kcalReplace = originalDesc.replace("Kcal", "calories");
-        var vegDesc = kcalReplace.replace("&", "and");
+        if (data != "Data Unavailable"){
+            var mainDishVeg = data.vegname.replace("&", "and");
+            var originalDesc = data.vegdesc;
+            var kcalReplace = originalDesc.replace("Kcal", "calories");
+            var vegDesc = kcalReplace.replace("&", "and");
 
-        speechOutput = mainDishVeg + ". " + vegDesc;
+            speechOutput = mainDishVeg + ". " + vegDesc;
 
-        
-        console.log(speechOutput);
-        // This is async and will run after the http call returns
-        response.say(speechOutput).shouldEndSession(true);
-        // Must call send to end the original request 
-        response.send(); 
+            
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }
+        else{
+            speechOutput = "Today's menu has not been uploaded yet. Please try again later"
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }   
            
     }).on('error', function (err) {
         console.log('something went wrong on the request', err.request.options);
@@ -220,20 +322,30 @@ app.intent('tellMeMoreMeatIntent', {
     client.get(url, function (lunch, callback) {
         // parsed response body as js object
         data = JSON.parse(lunch);
-        var mainDishOne = data.main1name.replace("&", "and");
-        var mainDishTwo = data.main2name.replace("&", "and");
-        var originalDesc1 = data.main1desc.replace("Kcal", "calories");
-        var mainOneDesc = originalDesc1.replace("&", "and");
-        var originalDesc2 = data.main2desc.replace("Kcal", "calories");
-        var mainTwoDesc = originalDesc2.replace("&", "and");
+        if (data != "Data Unavailable"){
+            var mainDishOne = data.main1name.replace("&", "and");
+            var mainDishTwo = data.main2name.replace("&", "and");
+            var originalDesc1 = data.main1desc.replace("Kcal", "calories");
+            var mainOneDesc = originalDesc1.replace("&", "and");
+            var originalDesc2 = data.main2desc.replace("Kcal", "calories");
+            var mainTwoDesc = originalDesc2.replace("&", "and");
 
-        speechOutput = mainDishOne + ". " + mainOneDesc + ". " + mainDishTwo + ". " + mainTwoDesc;
+            speechOutput = mainDishOne + ". " + mainOneDesc + ". " + mainDishTwo + ". " + mainTwoDesc;
 
-        console.log(speechOutput);
-        // This is async and will run after the http call returns
-        response.say(speechOutput).shouldEndSession(true);
-        // Must call send to end the original request 
-        response.send(); 
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }
+        else{
+            speechOutput = "Today's menu has not been uploaded yet. Please try again later"
+            console.log(speechOutput);
+            // This is async and will run after the http call returns
+            response.say(speechOutput).shouldEndSession(true);
+            // Must call send to end the original request 
+            response.send();
+        }    
            
     }).on('error', function (err) {
         console.log('something went wrong on the request', err.request.options);
