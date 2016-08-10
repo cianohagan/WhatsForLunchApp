d4f3ac9cef58662e683f14d24c34787840c91c63
cianohagan <cian.ohagan5@mail.dcu.ie>
2016-08-10 13:26:47 +0100

    Adding ability to overwrite documents in db

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def validateLogin():
     else:
         print "Incorrect Login Details"
         json.dumps("Incorrect Login Details")
-        return redirect('/showSignin')
+        return render_template('/incorrectsignin.html')
 
 @app.route('/logout')
 def logout():
@@ -57,6 +57,7 @@ def showAddWish():
 @app.route('/addWish',methods=['POST'])
 def addWish():
 
+    # Assigning inputted fields to variables
     _soup1name = request.form['inputSoup1Name']
     _soup1desc = request.form['inputSoup1Description']
     _soup1price = request.form['inputSoup1Price']
@@ -90,17 +91,30 @@ def addWish():
     else: # All other days need only one day to be added to date
         tomorrow_date = datetime.date.today() + datetime.timedelta(days=1)
 
+    # Deleting any previous entries with same date
+    map_fun = '''function(doc) {
+        if(doc.Date === "%s")
+            emit(doc.Date, doc);
+        }''' % tomorrow_date
+    for row in db_menulist.query(map_fun):
+        delete_menu_dict = row.value
+        id = delete_menu_dict["_id"]
+        doc_to_delete = db_menulist[id]
+        db_menulist.delete(doc_to_delete)
+
+    # Posting to CouchDB
     db_menulist.save({"soup1name": _soup1name, "soup1desc": _soup1desc, "soup1price": _soup1price,
                       "soup2name": _soup2name, "soup2desc": _soup2desc, "soup2price": _soup2price,
                       "main1name": _main1name, "main1desc": _main1desc, "main1price": _main1price,
                       "main2name": _main2name, "main2desc": _main2desc, "main2price": _main2price,
                       "vegname": _vegname, "vegdesc": _vegdesc, "vegprice": _vegprice,
-                      "Date": "%s"}) % tomorrow_date
+                      "Date": "%s" % tomorrow_date})
     return render_template('index.html')
 
 @app.route('/getWish')
 def getWish():
 
+    # Displaying Monday's menu if it is the weekend
     global today_date
     if(datetime.datetime.today().weekday() == 5): # If it's Sat add 2 days to date
         today_date = datetime.date.today() + datetime.timedelta(days=2)
